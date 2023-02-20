e0fc8bffc035658ed28607d87c02e42e4ac76904
palabhijit687 <palabhijit687@gmail.com>
2023-02-21 03:00:25 +0530

    show and add employee

---
+++ src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent {
+  employee: Employee = new Employee()
+  constructor(){}
+  ngOnInit(): void{
+
+  }
+  onSubmit(){
+    console.log(this.employee);
+  }
+}
